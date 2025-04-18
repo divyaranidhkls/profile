8a2956bde6fcde0814156906b6481e0c0a30f1ef
Divyarani K <divyaranidhkls@gmail.com>
2025-04-18 15:28:26 +0530

    Add AboutBlock component to display personal information and interests

---
+++ app/Blocks/About/AboutBlock.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import AboutInterests from './AboutInterests';
+import AboutText from './AboutText';
+
+const AboutBlock = () => {
+  return (
+    <div id= "about" className="container mx-auto">
+        <p className='text-4xl font-extrabold text-center mb-1 tracking-widest'>ABOUT ME</p>
+      <p className="text-center mb-4 italic pt-1">
+  Discover my professional interests, personal passions, and the hobbies that drive my creativity and continuous learning.
+</p>
+
+      <div className="flex flex-row justify-between items-start mt-10 ml-20 gap-10">
+        <div className="w-1/2">
+          <AboutText />
+        </div>
+        <div className="w-1/2">
+      
+          <AboutInterests />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutBlock;
