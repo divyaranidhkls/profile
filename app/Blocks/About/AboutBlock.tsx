69d12f266d892a43680c6d9ea75d054be3789d00
Divyarani K <divyaranidhkls@gmail.com>
2025-04-18 17:03:30 +0530

    Refactor AboutBlock component for improved layout and code consistency

--- app/Blocks/About/AboutBlock.tsx
+++ app/Blocks/About/AboutBlock.tsx
@@ -4,18 +4,19 @@ import AboutText from './AboutText';
 
 const AboutBlock = () => {
   return (
-    <div id= "about" className="container mx-auto">
-        <p className='text-4xl font-extrabold text-center mb-1 tracking-widest'>ABOUT ME</p>
+    <div id="about" className="container mx-auto px-4">
+      <p className="text-4xl font-extrabold text-center mb-1 tracking-widest">
+        ABOUT ME
+      </p>
       <p className="text-center mb-4 italic pt-1">
-  Discover my professional interests, personal passions, and the hobbies that drive my creativity and continuous learning.
-</p>
+        Discover my professional interests, personal passions, and the hobbies that drive my creativity and continuous learning.
+      </p>
 
-      <div className="flex flex-row justify-between items-start mt-10 ml-20 gap-10">
-        <div className="w-1/2">
+      <div className="flex flex-col md:flex-row justify-between items-start mt-10 gap-10">
+        <div className="w-full md:w-1/2">
           <AboutText />
         </div>
-        <div className="w-1/2">
-      
+        <div className="w-full md:w-1/2">
           <AboutInterests />
         </div>
       </div>
