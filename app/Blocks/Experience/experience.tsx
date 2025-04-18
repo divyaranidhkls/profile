025e68f56520ac51d990254c34bdb31a6cc8fafd
Divyarani K <divyaranidhkls@gmail.com>
2025-04-18 15:28:50 +0530

    Add Experience component to display skills and education

---
+++ app/Blocks/Experience/experience.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Skills from './skillls';
+import Education from './Education';
+
+ const  Experiance = () => {
+  return (
+    <div className="container max-auto">
+    <div className="flex flex-row justify-between mt-20 ml-20">
+    <div className='w-1/2'><Education/></div>
+    
+      <div className='w-1/2'><Skills/></div>
+      
+    </div>
+
+
+
+
+
+
+</div>
+  )
+}
+
+
+export default Experiance;
