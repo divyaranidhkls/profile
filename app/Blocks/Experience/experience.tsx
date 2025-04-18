e0c898e651d91927a48519194ec4791567b70a80
Divyarani K <divyaranidhkls@gmail.com>
2025-04-18 17:03:36 +0530

    Fix typo in Experience component name and improve layout structure for consistency

--- app/Blocks/Experience/experience.tsx
+++ app/Blocks/Experience/experience.tsx
@@ -1,25 +1,20 @@
-import React from 'react'
+import React from 'react';
 import Skills from './skillls';
 import Education from './Education';
 
- const  Experiance = () => {
+const Experience = () => {
   return (
-    <div className="container max-auto">
-    <div className="flex flex-row justify-between mt-20 ml-20">
-    <div className='w-1/2'><Education/></div>
-    
-      <div className='w-1/2'><Skills/></div>
-      
+    <div className="container mx-auto px-4">
+      <div className="flex flex-col md:flex-row justify-between mt-20 gap-10">
+        <div className="w-full md:w-1/2">
+          <Education />
+        </div>
+        <div className="w-full md:w-1/2">
+          <Skills />
+        </div>
+      </div>
     </div>
+  );
+};
 
-
-
-
-
-
-</div>
-  )
-}
-
-
-export default Experiance;
+export default Experience;
