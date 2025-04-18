6eb12f1038e8ac4ebbef72d2c873d954611bd31a
Divyarani K <divyaranidhkls@gmail.com>
2025-04-18 17:39:01 +0530

    Fix layout inconsistency by removing extra space in Experience component

--- app/Blocks/Experience/experience.tsx
+++ app/Blocks/Experience/experience.tsx
@@ -4,7 +4,7 @@ import Education from './Education';
 
 const Experience = () => {
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto px-4 ">
       <div className="flex flex-col md:flex-row justify-between mt-20 gap-10">
         <div className="w-full md:w-1/2">
           <Education />
